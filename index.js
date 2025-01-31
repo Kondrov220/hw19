bbfa260e7c345afbdda5c14f61f531dce0b3cfce
Kondrov220 <121194027+Kondrov220@users.noreply.github.com>
2025-01-31 21:48:53 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+//1
+const openButton = document.querySelector('button[data-action="open-modal"]');
+const closeButton = document.querySelector('button[data-action="close-modal"]');
+openButton.addEventListener("click", () => {
+document.body.classList.add("show-modal");
+});
+closeButton.addEventListener("click", () => {
+    document.body.classList.remove("show-modal");
+});
+//2
+const backdrop = document.querySelector(".backdrop");
+backdrop.addEventListener("click", () => {
+    document.body.classList.remove("show-modal");
+});
+//3
+const red = document.querySelector("input[value='red']");
+const white = document.querySelector("input[value='white']");
+const green = document.querySelector("input[value='green']");
+red.addEventListener("change", () => {
+    document.body.style.backgroundColor = "red";
+});
+white.addEventListener("change", () => {
+    document.body.style.backgroundColor = "white";
+});
+green.addEventListener("change", () => {
+    document.body.style.backgroundColor = "green";
+});
+//4
+const text = document.querySelector("input#name-input");
+const textCont = document.querySelector("span#name-output")
+text.addEventListener("input",  (e) => {
+    textCont.textContent = text.value.trim();
+   if(text.value.trim() === ""){
+   textCont.textContent = "незнайомець";
+}
+});
+const invalid = document.querySelector("input#validation-input");
+invalid.addEventListener("input", () => {
+    if(invalid.value.length >= 6){
+        invalid.classList.add("valid");
+        invalid.classList.remove("invalid");
+     }else{
+        invalid.classList.remove("valid");
+        invalid.classList.add("invalid");
+     }
+});
+//5
+const size = document.querySelector("input#font-size-control");
+size.addEventListener("input" , () => {
+document.querySelector("span#text").style.fontSize = `${size.value.trim()}px`;
+});
